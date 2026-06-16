8a1ff95141fed76cec89f8fad834c1bc6e18a80e
Christian <christian.huff.ch@gmail.com>
2026-06-16 16:46:01 +0100

    Add Spring Boot project

---
+++ src/main/java/io/betweendata/spring_boot_htmx_starter/SpringBootHtmxStarterApplication.java
@@ -0,0 +1,12 @@
+package io.betweendata.spring_boot_htmx_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHtmxStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHtmxStarterApplication.class, args);
+    }
+}
---
+++ src/test/java/io/betweendata/spring_boot_htmx_starter/SpringBootHtmxStarterApplicationTests.java
@@ -0,0 +1,11 @@
+package io.betweendata.spring_boot_htmx_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHtmxStarterApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
