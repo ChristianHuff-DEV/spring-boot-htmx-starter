9c552eb426454fcd9b368196ad0215256266cee3
Christian <christian.huff.ch@gmail.com>
2026-06-23 16:38:25 +0100

    Add HTMX

--- src/main/java/io/betweendata/spring_boot_htmx_starter/HomeController.java
+++ src/main/java/io/betweendata/spring_boot_htmx_starter/HomeController.java
@@ -1,15 +1,33 @@
 package io.betweendata.spring_boot_htmx_starter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private int counter = 0;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("counter", counter);
         return "index";
+    }
+
+    @PostMapping("/count")
+    public String count(@RequestParam String direction, Model model) {
+
+        if (direction.equals("up")) {
+            model.addAttribute("counter", ++counter);
+        } else {
+            model.addAttribute("counter", --counter);
+        }
+
+        return "components/counter";
     }
 }
